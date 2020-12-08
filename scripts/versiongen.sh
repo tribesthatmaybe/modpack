dd6e0abb373ce8e59ce8e7604fccf20788a29ee8
Jonathan Freedman <jonafree@gmail.com>
2020-12-07 19:11:57 -0800

    switch to new style avakas invokation

--- scripts/versiongen.sh
+++ scripts/versiongen.sh
@@ -9,35 +9,7 @@ problems() {
     exit 1
 }
 
-RELEASE_FILE="${ROOTDIR}/release"
-RELEASE=""
-if [ -e "$RELEASE_FILE" ] ; then
-    RELEASE="$(cat "$RELEASE_FILE")"
-fi
-if ! git describe --tags --exact-match "$(git rev-parse HEAD)" &> /dev/null ; then
-    docker run -v "${ROOTDIR}:/mnt" -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER \
-           otakup0pe/avakas show /mnt \
-	       --pre-build --pre-build-prefix="$RELEASE" --pre-build-date --build \
-		   2> /dev/null 1> "${ROOTDIR}/.version"
-else
-    if [ -z "$RELEASE" ] ; then
-        docker run -v "${ROOTDIR}:/mnt" -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER \
-               otakup0pe/avakas show /mnt \
-	           --pre-build --pre-build-prefix="$RELEASE" \
-			   2> /dev/null 1> "${ROOTDIR}/.version"
-    elif [ "$(git rev-parse --abbrev-ref HEAD)" != "mainline" ] ; then
-        # we might be on the latest tagged commit but in a dev branch
-        docker run -v "${ROOTDIR}:/mnt" -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER \
-               otakup0pe/avakas show /mnt \
-	           --pre-build --pre-build-prefix="$RELEASE" --pre-build-date --build \
-		       2> /dev/null 1> "${ROOTDIR}/.version"
-    else
-        docker run -v "${ROOTDIR}:/mnt" \
-               otakup0pe/avakas show /mnt \
-			   2> /dev/null 1> "${ROOTDIR}/.version"
-    fi
-fi
-
 docker run -v "${ROOTDIR}:/mnt" \
        otakup0pe/avakas show /mnt \
-	   2> /dev/null 1> "${ROOTDIR}/.version-container"
+	   2> /dev/null 1> "${ROOTDIR}/.version"
+cp "${ROOTDIR}/.version" "${ROOTDIR}/.version-container"
