d151f04fbbda5a9de2bcfba56d6b5afafdc859aa
Jonathan Freedman <jonafree@gmail.com>
2021-01-21 13:44:56 -0800

    trying to cache init in nbt

--- src/config/copy/customnpcs/scripts/ecmascript/party_kid.js
+++ src/config/copy/customnpcs/scripts/ecmascript/party_kid.js
@@ -82,11 +82,45 @@ function randomElement(someList, p_dupes) {
   return choices[s_number];
 }
 
+function populateLines(event, slot_id, lines) {
+  var w_lines = [];
+  for (var slot = 0; slot < 8; slot++) {
+    var w_line = randomElement(lines, w_lines);
+    if (w_line != null) {
+      event.npc.advanced.setLine(slot_id, slot, w_line, "");
+      w_lines.push(w_line);
+    }
+  }
+}
+
+function getFactionId(event, name) {
+  var factions = event.API.getFactions().list();
+  for (var i = 0 ; i < factions.length ; i++ ) {
+    if (factions[i].getName() == name) {
+      return factions[i].getId()
+    }
+  }
+  return null;
+}
+
 function init(event) {
-  event.npc.display.setSkinUrl(randomElement(partyKidSkins));
-  event.npc.display.setSkinTexture("customnpcs:textures/entity/humanmale/steve.png");
-  event.npc.display.setName(randomElement(partyKidNames));
-  event.npc.display.setShowName(1);
+  var npcNbt = event.npc.getEntityNbt();
+  var npcBirth = npcNbt.getInteger("ttmbNpcBirth");
+  var now = Math.floor(Date.now() / 1000);
+  if ( npcBirth == 0 ) {
+    event.npc.getStoreddata().clear();
+    var skinUrl = randomElement(partyKidSkins);
+    var name = randomElement(partyKidNames);
+    npcNbt.setInteger("ttmbNpcBirth", now);
+    event.npc.setEntityNbt(npcNbt);
+    event.npc.display.setSkinUrl(skinUrl);
+    event.npc.display.setSkinTexture("customnpcs:textures/entity/humanmale/steve.png");
+    event.npc.display.setName(name);
+    event.npc.display.setShowName(1);
+    event.npc.say("oh no");
+  } else {
+    event.npc.say("AGAIN after " + (now - npcBirth).toString() + "s");
+  }
 
   event.npc.stats.setMaxHealth(10);
   event.npc.stats.setAggroRange(5);
@@ -101,31 +135,15 @@ function init(event) {
   event.npc.ai.setWalkingSpeed(5);
   event.npc.ai.setMovingType(1);      // wandering
   event.npc.ai.setNavigationType(0);  // ground
-
-  var w_lines = [];
-  for (var slot = 0; slot < 8; slot++) {
-    var w_line = randomElement(partyKidWorldLines, w_lines);
-    if (w_line != null) {
-      event.npc.advanced.setLine(2, slot, w_line, "");
-      w_lines.push(w_line);
-    }
+  event.npc.ai.setDoorInteract(1);    // open
+  event.npc.ai.setReturnsHome(false); // used on reset/reload
+  event.npc.ai.setRetaliateType(2);   // retreat
+  event.npc.ai.setTacticalType(1);    // dodge
+  var factionId = getFactionId(event, "Cabaret Kids");
+  if (factionId != null) {
+    event.npc.setFaction(factionId);
   }
-  var p_lines = [];
-  for (var slot = 0; slot < 8; slot++) {
-    var p_line = randomElement(partyKidPlayerLines.concat(partyKidInteractLines), p_lines);
-    if (p_line != null) {
-      event.npc.advanced.setLine(0, slot, p_line, "");
-      p_lines.push(p_line);
-    }
-  }
-
-  var n_lines = [];
-  for (var slot = 0; slot < 8; slot++) {
-    var n_line = randomElement(partyKidNpcLines.concat(partyKidInteractLines), n_lines);
-    if (n_line != null) {
-      event.npc.advanced.setLine(5, slot, n_line, "");
-      n_lines.push(n_line);
-    }
-  }
-
+  populateLines(event, 2, partyKidWorldLines);
+  populateLines(event, 0, partyKidPlayerLines.concat(partyKidInteractLines));
+  populateLines(event, 5, partyKidNpcLines.concat(partyKidInteractLines));
 }
